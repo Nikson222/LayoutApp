da14c44dc33a15c067b58a65949c7f0626223170
Nikson222 <83268796+Nikson222@users.noreply.github.com>
2023-09-26 16:24:19 +0300

    Fix problem with loading with unknown scene name

--- Assets/_Scripts/SceneLoader.cs
+++ Assets/_Scripts/SceneLoader.cs
@@ -3,10 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string SceneName)
+    public void LoadScene(string sceneName)
     {
-        Scene sceneForLoad = SceneManager.GetSceneByName(SceneName);
-        if(sceneForLoad.IsValid())
-            SceneManager.LoadScene(SceneName);
+        if(SceneExists(sceneName))
+            SceneManager.LoadScene(sceneName);
+    }
+
+    private bool SceneExists(string sceneName)
+    {
+        string[] sceneNames = GetSceneNamesInBuild();
+
+        foreach (var name in sceneNames)
+        {
+            if (name == sceneName)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private string[] GetSceneNamesInBuild()
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        string[] sceneNames = new string[sceneCount];
+
+        for (int i = 0; i < sceneCount; i++)
+        {
+            sceneNames[i] = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+        }
+
+        return sceneNames;
     }
 }
